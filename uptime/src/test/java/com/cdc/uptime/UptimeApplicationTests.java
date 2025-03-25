c73b38d186b93ea2c681ffb7b770e8a8e34243a5
robin <gahlawatrobin30@gmail.com>
2025-03-26 00:31:37 +0530

    Initial commit

---
+++ uptime/src/test/java/com/cdc/uptime/UptimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdc.uptime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UptimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
